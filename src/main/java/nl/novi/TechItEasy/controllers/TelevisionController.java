9497d58f24842fd1f2e43056b8b1cea81f0252cd
edwinhelmus <edwin.helmus@home.nl>
2025-06-18 22:58:32 +0200

    put and patch

--- src/main/java/nl/novi/TechItEasy/controllers/TelevisionController.java
+++ src/main/java/nl/novi/TechItEasy/controllers/TelevisionController.java
@@ -3,8 +3,6 @@ package nl.novi.TechItEasy.controllers;
 import jakarta.validation.Valid;
 import nl.novi.TechItEasy.dtos.TelevisionRequestDto;
 import nl.novi.TechItEasy.dtos.TelevisionResponseDto;
-import nl.novi.TechItEasy.mappers.TelevisionMapper;
-import nl.novi.TechItEasy.models.Television;
 import nl.novi.TechItEasy.services.TelevisionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,13 +27,13 @@ public class TelevisionController {
     @GetMapping
     public ResponseEntity<List<TelevisionResponseDto>> getAllTelevisions(@RequestParam(value = "brand", required = false) String brand) {
 
-        List<TelevisionResponseDto> ResponseDtos = new ArrayList<>();
+        List<TelevisionResponseDto> responseDtos = new ArrayList<>();
         if (brand == null) {
-            ResponseDtos = televisionService.getAllTelevisions();
+            responseDtos = televisionService.getAllTelevisions();
         } else {
-            ResponseDtos = televisionService.getAllTelevisionsByBrand(brand);
+            responseDtos = televisionService.getAllTelevisionsByBrand(brand);
         }
-        return ResponseEntity.ok().body(ResponseDtos);
+        return ResponseEntity.ok().body(responseDtos);
     }
 
     @GetMapping("/{id}")
@@ -50,105 +48,23 @@ public class TelevisionController {
         return ResponseEntity.created(constructURI(responseDto.id)).body(responseDto);
     }
 
-//    @PutMapping("/{id}")
-//    public ResponseEntity<Object> updateTelevision(@PathVariable long id, @RequestBody Television television) {
-//
-//        Optional<Television> op = televisionRepository.findById(id);
-//        if (op.isPresent()) {
-//            Television television1 = op.get();
-//            television1.setAmbiLight(television.getAmbiLight());
-//            television1.setAvailableSize(television.getAvailableSize());
-//            television1.setAmbiLight(television.getAmbiLight());
-//            television1.setBluetooth(television.getBluetooth());
-//            television1.setBrand(television.getBrand());
-//            television1.setHdr(television.getHdr());
-//            television1.setName(television.getName());
-//            television1.setOriginalStock(television.getOriginalStock());
-//            television1.setPrice(television.getPrice());
-//            television1.setRefreshRate(television.getRefreshRate());
-//            television1.setScreenQuality(television.getScreenQuality());
-//            television1.setScreenType(television.getScreenType());
-//            television1.setSmartTv(television.getSmartTv());
-//            television1.setSold(television.getSold());
-//            television1.setType(television.getType());
-//            television1.setVoiceControl(television.getVoiceControl());
-//            television1.setWifi(television.getWifi());
-//
-//            Television returnTelevision = televisionRepository.save(television1);
-//            return ResponseEntity.ok().body(returnTelevision);
-//        } else {
-//            return ResponseEntity.notFound().build();
-//        }
-//    }
-//
-//
+    @PutMapping("/{id}")
+    public ResponseEntity<TelevisionResponseDto> updateTelevision(@PathVariable long id, @RequestBody TelevisionRequestDto televisionRequestDto) {
+        TelevisionResponseDto responseDto = this.televisionService.updateTelevision(id, televisionRequestDto);
+        return ResponseEntity.ok().body(responseDto);
+    }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<TelevisionResponseDto> deleteTelevision(@PathVariable("id") long id) {
         this.televisionService.deleteTelevision(id);
         return ResponseEntity.noContent().build();
     }
-//
-//    @PatchMapping("/{id}")
-//    public ResponseEntity<Television> updatePartialTelevision(@PathVariable Long id, @RequestBody Television newTelevision) {
-//        Optional<Television> television = televisionRepository.findById(id);
-//
-//        if (television.isEmpty()) {
-//            throw new RecordNotFoundException("No television found with id: " + id);
-//        } else {
-//            Television television1 = television.get();
-//            if (newTelevision.getAmbiLight() != null) {
-//                television1.setAmbiLight(newTelevision.getAmbiLight());
-//            }
-//            if (newTelevision.getAvailableSize() != null) {
-//                television1.setAvailableSize(newTelevision.getAvailableSize());
-//            }
-//            if (newTelevision.getBluetooth()) {
-//                television1.setBluetooth(newTelevision.getBluetooth());
-//            }
-//            if (newTelevision.getBrand() != null) {
-//                television1.setBrand(newTelevision.getBrand());
-//            }
-//            if (newTelevision.getHdr() != null) {
-//                television1.setHdr(newTelevision.getHdr());
-//            }
-//            if (newTelevision.getName() != null) {
-//                television1.setName(newTelevision.getName());
-//            }
-//            if (newTelevision.getOriginalStock() != null) {
-//                television1.setOriginalStock(newTelevision.getOriginalStock());
-//            }
-//            if (newTelevision.getPrice() != null) {
-//                television1.setPrice(newTelevision.getPrice());
-//            }
-//            if (newTelevision.getRefreshRate() != null) {
-//                television1.setRefreshRate(newTelevision.getRefreshRate());
-//            }
-//            if (newTelevision.getScreenQuality() != null) {
-//                television1.setScreenQuality(newTelevision.getScreenQuality());
-//            }
-//            if (newTelevision.getScreenType() != null) {
-//                television1.setScreenType(newTelevision.getScreenType());
-//            }
-//            if (newTelevision.getSmartTv() != null) {
-//                television1.setSmartTv(newTelevision.getSmartTv());
-//            }
-//            if (newTelevision.getSold() != null) {
-//                television1.setSold(newTelevision.getSold());
-//            }
-//            if (newTelevision.getType() != null) {
-//                television1.setType(newTelevision.getType());
-//            }
-//            if (newTelevision.getVoiceControl() != null) {
-//                television1.setVoiceControl(newTelevision.getVoiceControl());
-//            }
-//            if (newTelevision.getWifi() != null) {
-//                television1.setWifi(newTelevision.getWifi());
-//            }
-//
-//            Television returnTelevision = televisionRepository.save(television1);
-//            return ResponseEntity.ok().body(returnTelevision);
-//        }
-//    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<TelevisionResponseDto> updatePartialTelevision(@PathVariable Long id, @RequestBody TelevisionRequestDto televisionRequestDto) {
+        TelevisionResponseDto responseDto = this.televisionService.patchTelevision(id, televisionRequestDto);
+        return ResponseEntity.ok().body(responseDto);
+    }
 
 }
 
--- src/main/java/nl/novi/TechItEasy/services/TelevisionService.java
+++ src/main/java/nl/novi/TechItEasy/services/TelevisionService.java
@@ -55,9 +55,92 @@ public class TelevisionService {
         Television television = this.televisionRepository.findById(id)
               .orElseThrow(() -> new ResourceNotFoundException("Television " + id + " not found"));
         this.televisionRepository.deleteById(id);
-        return;
     }
 
+    public TelevisionResponseDto updateTelevision(Long id, TelevisionRequestDto televisionRequestDto) {
+        Television television = this.televisionRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Television " + id + " not found"));
 
+        Television televisionNew = TelevisionMapper.toEntity(televisionRequestDto);
+        television.setAmbiLight(televisionNew.getAmbiLight());
+        television.setAvailableSize(televisionNew.getAvailableSize());
+        television.setAmbiLight(televisionNew.getAmbiLight());
+        television.setBluetooth(televisionNew.getBluetooth());
+        television.setBrand(televisionNew.getBrand());
+        television.setHdr(televisionNew.getHdr());
+        television.setName(televisionNew.getName());
+        television.setOriginalStock(televisionNew.getOriginalStock());
+        television.setPrice(televisionNew.getPrice());
+        television.setRefreshRate(televisionNew.getRefreshRate());
+        television.setScreenQuality(televisionNew.getScreenQuality());
+        television.setScreenType(televisionNew.getScreenType());
+        television.setSmartTv(televisionNew.getSmartTv());
+        television.setSold(televisionNew.getSold());
+        television.setType(televisionNew.getType());
+        television.setVoiceControl(televisionNew.getVoiceControl());
+        television.setWifi(televisionNew.getWifi());
+
+        Television returnTelevision = this.televisionRepository.save(television);
+        return TelevisionMapper.toResponseDto(returnTelevision);
+    }
+
+    public TelevisionResponseDto patchTelevision(Long id, TelevisionRequestDto televisionRequestDto) {
+        Television television = this.televisionRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Television " + id + " not found"));
+
+        Television televisionNew = TelevisionMapper.toEntity(televisionRequestDto);
+
+        if (televisionNew.getAmbiLight() != null) {
+            television.setAmbiLight(televisionNew.getAmbiLight());
+        }
+        if (televisionNew.getAvailableSize() != null) {
+            television.setAvailableSize(televisionNew.getAvailableSize());
+        }
+        if (televisionNew.getBluetooth() != null) {
+            television.setBluetooth(televisionNew.getBluetooth());
+        }
+        if (televisionNew.getBrand() != null) {
+            television.setBrand(televisionNew.getBrand());
+        }
+        if (televisionNew.getHdr() != null) {
+            television.setHdr(televisionNew.getHdr());
+        }
+        if (televisionNew.getName() != null) {
+            television.setName(televisionNew.getName());
+        }
+        if (televisionNew.getOriginalStock() != null) {
+            television.setOriginalStock(televisionNew.getOriginalStock());
+        }
+        if (televisionNew.getPrice() != null) {
+            television.setPrice(televisionNew.getPrice());
+        }
+        if (televisionNew.getRefreshRate() != null) {
+            television.setRefreshRate(televisionNew.getRefreshRate());
+        }
+        if (televisionNew.getScreenQuality() != null) {
+            television.setScreenQuality(televisionNew.getScreenQuality());
+        }
+        if (televisionNew.getScreenType() != null) {
+            television.setScreenType(televisionNew.getScreenType());
+        }
+        if (televisionNew.getSmartTv() != null) {
+            television.setSmartTv(televisionNew.getSmartTv());
+        }
+        if (televisionNew.getSold() != null) {
+            television.setSold(televisionNew.getSold());
+        }
+        if (televisionNew.getType() != null) {
+            television.setType(televisionNew.getType());
+        }
+        if (televisionNew.getVoiceControl() != null) {
+            television.setVoiceControl(televisionNew.getVoiceControl());
+        }
+        if (televisionNew.getWifi() != null) {
+            television.setWifi(televisionNew.getWifi());
+        }
+
+        Television returnTelevision = this.televisionRepository.save(television);
+        return TelevisionMapper.toResponseDto(returnTelevision);
+    }
 }
 
